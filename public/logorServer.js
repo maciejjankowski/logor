6fb12d814cda4220b1d7e5debfd37c4c7e1975a4
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2014-02-16 19:32:47 +0100

    komunikaty i pingi na jednym kanale

---
+++ public/logorServer.js
@@ -0,0 +1,95 @@
+/**
+ * Created by mj on 16.02.14.
+ */
+var socket = io.connect(location.protocol + "//" + location.host);//http://cbping.azurewebsites.net/
+socket.on('connect', function(socket){
+  console.info("connected");
+});
+
+socket.on('msg', function (data) {
+//  console.log('msg', data)
+  showMessage(data);
+});
+
+socket.on('ping', function(data){
+  isOk(data.name);
+  clearTimeout( notOkArr[data.name] );
+  clearTimeout( idleArr[data.name] );
+
+  idleArr[data.name] = setTimeout(function(){
+    idle(data.name);
+  }, 333);
+
+  notOkArr[data.name] = setTimeout(function(){
+    notOk(data.name);
+  }, data.timeout || sessionStorage.getItem("idle") || notOkTimeout);
+
+});
+
+
+function showMessage(data){
+  var f = $("#out" );
+  var msgs = f.html().split("\n" );
+
+  if ( msgs.length > 200){
+    msgs.pop();
+  }
+
+  var msg = data.time + ' <span class="' + data.type + '" data-source="'+ data.name +'">' +"["+ data.name +"] "+  data.body + '</span>';
+  msgs.unshift(msg);
+
+  f.html( msgs.join("\n") );
+} // showMessage
+
+
+var notOkArr = [];
+var notOkTimeout = 15*60*1000; // 15 minut
+
+var idleArr = [];
+
+function idle(el){
+  var f = document.querySelector( "#" + el );
+  f.setAttribute('class', 'idle');
+} // idle
+
+
+function notOk(el){
+  var f = document.querySelector( "#" + el );
+  f.setAttribute('class', 'not-ok');
+  try{
+    if (!sessionStorage.getItem("mute"))
+      document.querySelector("#hlaps").play();
+  } catch(e){
+    console.error('hlaps')
+  }
+
+} // notOk
+
+function isOk(el){
+
+  var f = document.querySelector( "#" + el );
+  if (!f){
+    $('#pings').append('<span id="' + el + '">'+ el +'</span>');
+  }
+
+  $( "#" + el ).attr('class', 'ok');
+} // isOk
+
+$( document ).ready(function() {
+  $("#unmute" ).toggle();
+
+  $("#mute").on('click', function(){
+    sessionStorage.setItem("mute",1);
+    $("#unmute" ).toggle();
+    $("#mute" ).toggle();
+  });
+
+  $("#unmute").on('click', function(){
+    sessionStorage.setItem("mute","");
+    $("#unmute" ).toggle();
+    $("#mute" ).toggle();
+  });
+
+});
+
+
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 /**
  * Created by mj on 02.02.14.
- */
-/**
 
  https://github.com/flatiron/winston
  logs.papertrailapp.com:28135
  * */
-
-
+{
 var express = require('express');
 var fs = require('fs');
 
@@ -24,23 +21,87 @@ var server = app.listen(process.env.port || nconf.get("port") || "3210");
 
 var io = require('socket.io').listen(server)
     .set('log level', 0)
+}
+function ts(){
+  return new Date( (new Date().getTime()) + 1000 * 60 * 60).toJSON().replace("T", " ").replace("Z",""); // czy będzie działał po zmianie czasu?
+}
 
-io.of('/msg').on('connection', function(socket){
 
-  socket.send("hello");
+io.on('connection',function(socket){
 
-  socket.on("message", function (data){
-    socket.broadcast.send(data);
-    fs.appendFile('logs/log.txt', (new Date().toJSON().replace("T", " ").replace("Z","")) + " "+data + "\n");
+  socket.emit('msg', {time: ts(), body:'hello', name:'SERVER'});
+
+  socket.on('ping', function(data) {
+
+    socket.set('name', data.name);
+
+    if (!data.timeout){
+      socket.get('notOkTimeout', function(err, timeout){
+        data.timeout = timeout || nconf.get('default_timeout') || 15 * 60e3;
+        io.sockets.emit('ping', data);
+      });
+    }
+    else {
+      io.sockets.emit('ping', data);
+      socket.set('notOkTimeout', data.timeout);
+    }
+
+//    console.log(data.name + ' is alive');
   });
-});
 
-io.of('/ping' ).on('connection',function(socket){
-  socket.on('ping', function(data){
-    io.sockets.emit('ping', data);
-    io.of('/ping').emit('ping', data);
-    console.log(data.name + ' is alive');
-  })
-})
+  socket.on('msg', function(data){
+
+//    console.log('msg rcvd');
+
+    socket.get('name', function(err, name){
+//      console.log(name, " is sending a message");
+      data.name = name;
+      socket.broadcast.send(data);
+      socket.broadcast.emit('msg', data); // powinno być raczej emit
+    });
+
+// TODO : logowanie do bazy? i może nawet pobieranie paczek ze wszystkimi logami o danej nazwie
+//    fs.appendFile('logs/log.txt', (new Date().toJSON().replace("T", " ").replace("Z","")) + " "+data + "\n");
+  });
+
+});
+// ------------ NEW CODE ----------------------
 
 console.log("listening on: " + server.address().port );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
